6c31129baedc0c334e58195f75c02a26548fdd22
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-04-19 22:32:22 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# paopao-pref
+# PaoPao-Pref
+## 简介
+这是一个让DNS服务器预读取缓存或者压力测试的简单工具，配合[PaoPaoDNS](https://github.com/kkkgo/PaoPaoDNS)使用可以快速生成`redis_dns.rdb`缓存。从指定的文本读取域名列表并调用nslookup命令查询记录，docker镜像默认自带了全球前100万热门域名。   
+## 警告
+- 测试可能会对你的网络造成负担，请避免在网络正常使用时段进行测试。
+- 若配合[PaoPaoDNS](https://github.com/kkkgo/PaoPaoDNS)使用，如果设置了`CNAUTO=yes`，测试前请务必设置PaoPaoDNS的docker镜像的环境变量`CNAUTO=yes`和`CNFALL=no`。
+- 若配合[PaoPaoDNS](https://github.com/kkkgo/PaoPaoDNS)使用，建议PaoPao DNS的Docker的可用内存≥6 GB，否则无法缓存所有域名。
+- 不建议设置过高的并发，可能会导致你的服务器崩溃或者DNS无法被有效缓存。
+## 命令行参数
+参数选项|值|作用
+-|-|-|
+-file|文件路径|指定域名列表，默认值为目录下的`domains.txt`.
+-limit|并发数|指定并发数，默认值为10.
+-server|DNS服务器|指定DNS服务器，默认值为空.
+-line|行数|指定从第几行开始，可用于恢复进度.
+-v|开关|输出域名的查询信息.
+-h|开关|显示帮助信息.
+
+## 使用二进制文件
+可以从[Release](https://github.com/kkkgo/PaoPao-Pref/releases)下载对应平台编译好的二进制文件，压缩包内已经附带最新热门100万域名列表。
+## 使用Docker镜像
+```shell
+# 帮助信息
+docker run --rm -it sliamb/paopao-pref -h
+# 指定DNS服务器为192.168.1.8
+docker run --rm -it sliamb/paopao-pref -server 192.168.1.8
+# 从第1000行开始
+docker run --rm -it sliamb/paopao-pref -line 1000 -server 192.168.1.8
+# 指定并发数为5
+docker run --rm -it sliamb/paopao-pref -limit 5 -server 192.168.1.8
+```
+## 测试数据参考
+PaoPaoDNS：4核心8G内存   
+并发：10   
+域名数据：100万  
+运行耗时：39小时   
+生成的`redis_dns.rdb`缓存文件大小：917 MB    
+`used_memory_human:1.06G`
+
+## 附录
+域名数据来源： https://s3-us-west-1.amazonaws.com/umbrella-static/index.html   
---
+++ release.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+sh update_data.sh
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o paopao-perf
+tar -czvf 2_linux_amd64_paopao-perf.tar.gz paopao-perf domains.txt
+
+CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o paopao-perf
+tar -czvf 2_linux_386_paopao-perf.tar.gz paopao-perf domains.txt
+
+CGO_ENABLED=0 GOOS=4_freebsd GOARCH=386 go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o paopao-perf
+tar -czvf 4_freebsd_386_paopao-perf.tar.gz paopao-perf domains.txt
+
+CGO_ENABLED=0 GOOS=4_freebsd GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o paopao-perf
+tar -czvf 4_freebsd_amd64_paopao-perf.tar.gz paopao-perf domains.txt
+
+CGO_ENABLED=0 GOOS=4_freebsd GOARCH=arm go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o paopao-perf
+tar -czvf 4_freebsd_arm_paopao-perf.tar.gz paopao-perf domains.txt
+
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o paopao-perf
+tar -czvf 2_linux_arm_v7_paopao-perf.tar.gz paopao-perf domains.txt
+
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=6 go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o paopao-perf
+tar -czvf 2_linux_arm_v6_paopao-perf.tar.gz paopao-perf domains.txt
+
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=5 go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o paopao-perf
+tar -czvf 2_linux_arm_v5_paopao-perf.tar.gz paopao-perf domains.txt
+
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o paopao-perf
+tar -czvf 2_linux_arm64_paopao-perf.tar.gz paopao-perf domains.txt
+
+CGO_ENABLED=0 GOOS=linux GOARCH=mips64 go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o paopao-perf
+tar -czvf 2_linux_mips64_paopao-perf.tar.gz paopao-perf domains.txt
+
+CGO_ENABLED=0 GOOS=linux GOARCH=mips64le go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o paopao-perf
+tar -czvf 2_linux_mips64le_paopao-perf.tar.gz paopao-perf domains.txt
+
+CGO_ENABLED=0 GOOS=linux GOARCH=mipsle go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o paopao-perf
+tar -czvf 2_linux_mipsle_paopao-perf.tar.gz paopao-perf domains.txt
+
+CGO_ENABLED=0 GOOS=linux GOARCH=mips go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o paopao-perf
+tar -czvf 2_linux_mips_paopao-perf.tar.gz paopao-perf domains.txt
+
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o paopao-perf.exe
+tar -czvf 1_windows_386_paopao-perf.tar.gz paopao-perf.exe domains.txt
+
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o paopao-perf.exe
+tar -czvf 1_windows_amd64_paopao-perf.tar.gz paopao-perf.exe domains.txt
+
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" -trimpath -o paopao-perf
+tar -czvf 3_darwin_amd64_paopao-perf.tar.gz paopao-perf domains.txt
+
+mkdir -p ./build/
+mv *.tar.gz ./build/
