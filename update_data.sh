908c71431deb47d83f4aec0d7cf5e73197b5fe90
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-04-22 04:21:26 +0800

    Update update_data.sh

--- update_data.sh
+++ update_data.sh
@@ -4,7 +4,7 @@ unzip top-1m.csv.zip
 cut -d"," -f2  top-1m.csv > domains.txt
 rm top-1m.csv.zip top-1m.csv
 sudo apt-get -qq -y install golang
-go build -ldflags "-s -w" -trimpath -o /usr/bin/paopao-pref
+go build -ldflags "-s -w" -trimpath -o ./paopao-pref
 export FILE_OUTPUT=yes
 export DNS_LIMIT=100
 touch domains_ok.txt
