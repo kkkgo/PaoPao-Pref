a983bd8edefe6a0207af4f070ad88e7631980d4d
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-04-22 04:31:00 +0800

    Update update_data.sh

--- update_data.sh
+++ update_data.sh
@@ -3,14 +3,14 @@ curl -sLo top-1m.csv.zip http://s3-us-west-1.amazonaws.com/umbrella-static/top-1
 unzip top-1m.csv.zip
 cut -d"," -f2  top-1m.csv > domains.txt
 rm top-1m.csv.zip top-1m.csv
-# sudo apt-get -qq -y install golang
-# go build -ldflags "-s -w" -trimpath -o ./paopao-pref
-# export FILE_OUTPUT=yes
-# export DNS_LIMIT=100
-# touch domains_ok.txt
-# chmod +x ./paopao-pref
-# ./paopao-pref
-# count=$(cat domains_ok.txt|wc -l)
-# if [ "$count" -gt 100000 ];then
-# mv domains_ok.txt domains.txt
-# fi
+sudo apt-get -qq -y install golang
+go build -ldflags "-s -w" -trimpath -o ./paopao-pref
+export FILE_OUTPUT=yes
+export DNS_LIMIT=200
+touch domains_ok.txt
+chmod +x ./paopao-pref
+./paopao-pref
+count=$(cat domains_ok.txt|wc -l)
+if [ "$count" -gt 100000 ];then
+mv domains_ok.txt domains.txt
+fi
