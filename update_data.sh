8d1bbb3a806df65d2020ab2a4ff9142db29e4d12
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-06-11 01:58:21 +0800

    Update update_data.sh

--- update_data.sh
+++ update_data.sh
@@ -6,7 +6,7 @@ rm top-1m.csv.zip top-1m.csv
 sudo apt-get -qq -y install golang
 go build -ldflags "-s -w" -trimpath -o ./paopao-pref
 export FILE_OUTPUT=yes
-export DNS_LIMIT=25
+export DNS_LIMIT=12
 export DNS_SLEEP=0ms
 export DNS_TIMEOUT=3s
 touch domains_ok.txt
