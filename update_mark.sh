124b4a57f534e3ed61e9884a4e702e01f41c5359
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-06-23 12:34:30 +0800

    global_mark

--- update_mark.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker run -d --name mark_builder sliamb:/mark_builder
-rm global_mark.dat global_mark.dat.sha256sum
-docker cp mark_builder:/pub/global_mark.dat .
-docker cp mark_builder:/pub/global_mark.dat.sha256sum .
