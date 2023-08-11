aa26d99ce59961b984418356a074d02cffc22bdf
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-08-11 22:40:03 +0800

    test

--- dnscrypt_resolver/check.sh
+++ dnscrypt_resolver/check.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get -qq install dnscrypt-proxy
+sudo apt-get -qq install dnscrypt-proxy git
+git clone https://github.com/DNSCrypt/dnscrypt-resolvers.git --depth 1 /dnscrypt-resolvers
+
