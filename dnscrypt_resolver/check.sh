754002c360378d8179b2863508adce017342eabb
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-08-12 00:17:13 +0800

    d test

--- dnscrypt_resolver/check.sh
+++ dnscrypt_resolver/check.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-sudo apt-get update
-sudo apt-get -qq install dnscrypt-proxy git dnsutils
 git clone https://github.com/DNSCrypt/dnscrypt-resolvers.git --depth 1 /tmp/dnscrypt-resolvers
 grep -E "##" /tmp/dnscrypt-resolvers/v3/public-resolvers.md >/tmp/dnscrypt-resolvers/dnstest_alldns.txt
 cut -d" " -f2 /tmp/dnscrypt-resolvers/dnstest_alldns.txt | sort -u >/tmp/name_list.txt
@@ -14,25 +12,23 @@ grep -v "#" /tmp/dnscrypt-proxy/dnscrypt-proxy/example-dnscrypt-proxy.toml | gre
 sed -i -r "s/listen_addresses.+/listen_addresses = ['0.0.0.0:5302']/g" /tmp/dnsex.toml
 sed -i -r "s/^server_names.+//g" /tmp/dnsex.toml
 cat /tmp/dnsex.toml
-type dnscrypt-proxy
-sudo /usr/sbin/dnscrypt-proxy -config /tmp/dnsex.toml &
+dnscrypt-proxy -config /tmp/dnsex.toml &
 sleep 5
 
 local_lookup() {
-    sudo killall dnscrypt-proxy
+    killall dnscrypt-proxy
     server_name=$1
     domain_name=$2
     sed "1i server_names = [ '$server_name' ]" /tmp/dnsex.toml >/tmp/test_now.toml
-    sudo /usr/sbin/dnscrypt-proxy -config /tmp/test_now.toml &
-    sleep 1
+    dnscrypt-proxy -config /tmp/test_now.toml &
     test_res=$(dig @127.0.0.1 -p5302 "$domain_name")
-    sudo killall dnscrypt-proxy
+    killall dnscrypt-proxy
     echo "$test_res"
 }
 
 # test
 touch /tmp/bad_new.txt
-cat dnscrypt_resolver/ban_list.txt >>/tmp/bad_new.txt
+cat /data/ban_list.txt >>/tmp/bad_new.txt
 testrec=$(nslookup local.03k.org)
 if echo "$testrec" | grep -q "10.9.8.7"; then
     echo "Ready to test..."
@@ -54,4 +50,4 @@ if echo "$testrec" | grep -q "10.9.8.7"; then
 else
     echo "Test record failed.""$testrec"
 fi
-sort -u /tmp/bad_new.txt | grep -E "[a-z]" >dnscrypt_resolver/ban_list.txt
+sort -u /tmp/bad_new.txt | grep -E "[a-z]" >/data/ban_list.txt
