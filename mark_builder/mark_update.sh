a33ad91ff15fd504a60febdd3f15806e8ad23e7f
kissshot <4530897+kkkgo@users.noreply.github.com>
2023-11-17 01:52:18 +0800

    update mark runner.

--- mark_builder/mark_update.sh
+++ mark_builder/mark_update.sh
@@ -1,98 +1,50 @@
 #!/bin/sh
 IPREX4='([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
 
+# gen test_cn for /tmp/gen.yaml
 gen_dns() {
-    if [ "$SYSDNS" = "yes" ]; then
-        cat /etc/resolv.conf | grep -Eo "$IPREX4" >/tmp/dns_list.txt
-    else
-        touch /tmp/delay.txt
-        while read dnsserver; do
-            echo "Test "$dnsserver
-            sed "s/{ser1}/$dnsserver/g" test_cn.yaml | sed "s/#dns_check//g" >/tmp/test_cn.yaml
-            mosdns start -d /tmp -c test_cn.yaml >/dev/null 2>&1 &
-            sleep 1
-            delay=$(paopao-pref -server 127.0.0.1 -port 5304 -delay) && echo "$delay"",""$dnsserver" >>/tmp/delay.txt && echo "$dnsserver"": ""$delay"" ms"
-            killall mosdns
-        done <dns_list.txt
-        cat /tmp/delay.txt
-        sort -n /tmp/delay.txt | cut -d "," -f2 | head -3 >/tmp/dns_list.txt
-    fi
-    touch /tmp/force_list.txt
+    cat /data/dns_list.txt /etc/resolv.conf | grep -Eo "$IPREX4" >>/tmp/dns_list.txt
+    touch /tmp/delay.txt
     while read dnsserver; do
-        sed "s/{ser1}/$dnsserver/g" test_cn.yaml | sed "s/#dns_check//g" >/tmp/test_cn.yaml
+        echo "Test "$dnsserver
+        sed "s/1.2.3.4/$dnsserver/g" test_cn.yaml >/tmp/test_cn.yaml
         mosdns start -d /tmp -c test_cn.yaml >/dev/null 2>&1 &
         sleep 1
-        paopao-pref -server 127.0.0.1 -port 5304 -delay -v
+        delay=$(paopao-pref -server 127.0.0.1 -port 5301 -delay) && echo "$delay"",""$dnsserver" >>/tmp/delay.txt && echo "$dnsserver"": ""$delay"" ms"
         killall mosdns
     done </tmp/dns_list.txt
-    ser_num=$(cat /tmp/dns_list.txt | wc -l)
+    cat /tmp/delay.txt
+    sort -n /tmp/delay.txt | cut -d "," -f2 | head -3 >/tmp/dns_list_gen.txt
+    while read dnsserver; do
+        sed "s/1.2.3.4/$dnsserver/g" test_cn.yaml >/tmp/test_cn.yaml
+        mosdns start -d /tmp -c test_cn.yaml >/dev/null 2>&1 &
+        sleep 1
+        paopao-pref -server 127.0.0.1 -port 5301 -delay -v
+        killall mosdns
+    done </tmp/dns_list_gen.txt
+    ser_num=$(cat /tmp/dns_list_gen.txt | wc -l)
     if [ "$ser_num" = "0" ]; then
         echo "no dns available."
-        cat /tmp/dns_list.txt
+        cat /tmp/dns_list_gen.txt
         exit
     fi
-    ser1=$(head -1 /tmp/dns_list.txt)
-    ser2=$(head -2 /tmp/dns_list.txt | tail -1)
-    ser3=$(tail -1 /tmp/dns_list.txt)
+    ser1=$(head -1 /tmp/dns_list_gen.txt)
+    ser2=$(head -2 /tmp/dns_list_gen.txt | tail -1)
     cp test_cn.yaml /tmp/gen.yaml
-    if [ "$ser_num" -gt 0 ]; then
-        sed -i "s/{ser1}/$ser1/g" /tmp/gen.yaml
-    fi
-
+    sed -i "s/2.3.4.5/$ser1/g" /tmp/gen.yaml
     if [ "$ser_num" -gt 1 ]; then
-        sed -i "s/#ser_num2//g" /tmp/gen.yaml
-        sed -i "s/{ser2}/$ser2/g" /tmp/gen.yaml
-    fi
-
-    if [ "$ser_num" -gt 2 ]; then
-        sed -i "s/#ser_num3//g" /tmp/gen.yaml
-        sed -i "s/{ser3}/$ser3/g" /tmp/gen.yaml
-    fi
-}
-
-gen_nocn_list() {
-    if [ ! -d "/predata/" ]; then
-        cat /data/fwrule.txt >/tmp/force_list.txt
+        sed -i "s/3.4.5.6/$ser2/g" /tmp/gen.yaml
     else
-        cat /predata/force_list_global.txt >/tmp/force_list.txt
-    fi
-}
-
-gen_cn_list() {
-    if [ ! -d "/predata/" ]; then
-        echo "" >/tmp/force_list.txt
-    else
-        cat /predata/force_list_cn.txt >/tmp/force_list.txt
+        sed -i "/3.4.5.6/d" /tmp/gen.yaml
     fi
 }
 
 mosdns_gen_mark_start() {
+    killall mosdns
     sed "s/#gen_mark//g" /tmp/gen.yaml >/tmp/gen_mark.yaml
     mosdns start -d /tmp -c gen_mark.yaml &
     sleep 1
     ps
-}
-
-mosdns_gen_cn_start() {
-    killall mosdns
-    sleep 1
-    sed "s/#icp_mark//g" /tmp/gen.yaml >/tmp/icp_mark.yaml
-    mosdns start -d /tmp -c icp_mark.yaml &
-    sleep 1
-    ps
-}
-
-gen_pfdata_mark() {
-    if [ ! -d "/predata/" ]; then
-        cat /data/fwrule.txt >/tmp/pfdata.txt
-        echo "" >>/tmp/pfdata.txt
-        echo "" >>/tmp/pfdata.txt
-        cat /data/domains.txt >>/tmp/pfdata.txt
-        echo "" >>/tmp/pfdata.txt
-        paopao-pref -inrule /tmp/pfdata.txt -outrule /data/pfdata.txt
-    else
-        cat /predata/pfdata.txt >/data/pfdata.txt
-    fi
 }
 
 pref_start_mark() {
@@ -101,7 +53,7 @@ pref_start_mark() {
         rm domains_ok.txt
         touch domains_ok.txt
     fi
-    paopao-pref -file /data/pfdata.txt -server 127.0.0.1 -port 5304 -v >/tmp/pref.log
+    paopao-pref -file /data/topdomains.rules -server 127.0.0.1 -port 5302 -v >/tmp/pref.log
 }
 
 pref_start_cn() {
@@ -110,63 +62,33 @@ pref_start_cn() {
         rm domains_ok.txt
         touch domains_ok.txt
     fi
-    paopao-pref -file /data/domains_raw.txt -server 127.0.0.1 -port 5304 -v >/tmp/pref.log
+    paopao-pref -file /data/topdomains.txt -server 127.0.0.1 -port 5303 -v >/tmp/pref.log
 }
 
-global_debug() {
-    mkdir -p /pub/debug/global
-    cp /tmp/pref.log /pub/debug/global/
-    cp /data/domains_ok.txt /pub/debug/global/
-    cp /data/pfdata.txt /pub/debug/global/
-    cp /data/domains_raw.txt /pub/debug/
-}
-
-global_cn() {
-    mkdir -p /pub/debug/cn
-    cp /tmp/pref.log /pub/debug/cn/
-    cp /data/domains_ok.txt /pub/debug/cn/
-}
-
-gen_dat() {
-    if [ ! -d "/predata/" ]; then
-        cat /data/fwrule.txt >/tmp/global_mark.txt
-        echo "" >>/tmp/global_mark.txt
-        echo "" >>/tmp/global_mark.txt
-        cat /data/domains_ok.txt >>/tmp/global_mark.txt
-        echo "" >>/tmp/global_mark.txt
-        paopao-pref -inrule /tmp/global_mark.txt -outrule /data/global_mark.dat
-    else
-        cat /predata/tlds.txt >/tmp/global_mark.txt
-        echo "" >>/tmp/global_mark.txt
-        cat /data/fwrule.txt >>/tmp/global_mark.txt
-        echo "" >>/tmp/global_mark.txt
-        cat /data/domains_ok.txt >>/tmp/global_mark.txt
-        echo "" >>/tmp/global_mark.txt
-        paopao-pref -inrule /tmp/global_mark.txt -outrule /data/global_mark_global.dat
-        if [ "$TEST" = "debug" ]; then
-            paopao-pref -an -inrule /data/global_mark_global.dat -outrule /pub/debug/global/global_mark_global_analyze.txt
-        fi
-        gen_cn_list
-        mosdns_gen_cn_start
-        pref_start_cn
-        if [ "$TEST" = "debug" ]; then
-            global_cn
-        fi
+gen_global() {
+    echo "" >>/data/global.rules
+    cat /data/domains_ok.txt >>/data/global.rules
+    echo "" >>/data/global.rules
+    paopao-pref -inrule /data/global.rules -outrule /data/global_mark_global.dat
+    if [ "$TEST" = "debug" ]; then
+        paopao-pref -an -inrule /data/global_mark_global.dat -outrule /pub/debug/global/global_mark_global_analyze.txt
     fi
-
-    cat /predata/cn.txt >/tmp/global_mark_cn.txt
+}
+gen_cn() {
+    cat /data/global.cnfilter.rules >/tmp/global_mark_cn.txt
+    echo "" >>/tmp/global_mark_cn.txt
+    cat /data/cn.rules >>/tmp/global_mark_cn.txt
     echo "" >>/tmp/global_mark_cn.txt
     cat /data/domains_ok.txt >>/tmp/global_mark_cn.txt
     echo "" >>/tmp/global_mark_cn.txt
     paopao-pref -inrule /tmp/global_mark_cn.txt -outrule /data/global_mark_cn.dat
-
     if [ "$TEST" = "debug" ]; then
         paopao-pref -an -inrule /data/global_mark_cn.dat -outrule /pub/debug/cn/global_mark_cn_analyze.txt
     fi
-    paopao-pref -gbfile /data/global_mark_global.dat -cnfile /data/global_mark_cn.dat -comp /data/global_mark.dat
 }
 
 hash_dat() {
+    paopao-pref -gbfile /data/global_mark_global.dat -grfile /data/global.rules -crfile /data/cn.rules -cnfile /data/global_mark_cn.dat -comp /data/global_mark.dat
     cd /data || exit
     if [ "$TEST" = "debug" ]; then
         cp global_mark.dat /pub/global_mark_raw.dat
@@ -182,13 +104,12 @@ hash_dat() {
 }
 
 gen_dns
-gen_nocn_list
 mosdns_gen_mark_start
-gen_pfdata_mark
 pref_start_mark
+gen_global
+pref_start_cn
+gen_cn
 if [ "$TEST" = "debug" ]; then
     global_debug
 fi
-
-gen_dat
 hash_dat
