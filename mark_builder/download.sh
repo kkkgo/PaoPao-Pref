7ce3eed89c9a04e89640e55db93002913fbd9bf8
kissshot <4530897+kkkgo@users.noreply.github.com>
2026-02-15 17:38:59 +0800

    Update download.sh

--- mark_builder/download.sh
+++ mark_builder/download.sh
@@ -3,9 +3,11 @@
 mkdir -p /data
 
 # Get the latest mmdb: /data/Country-only-cn-private.mmdb
-curl -sLo /data/Country-only-cn-private.mmdb https://raw.githubusercontent.com/kkkgo/Country-only-cn-private.mmdb/main/Country-only-cn-private.mmdb &&
+git clone --depth 1 -b main https://github.com/kkkgo/Country-only-cn-private.mmdb /data/mmdb_repo &&
+    cp /data/mmdb_repo/Country-only-cn-private.mmdb /data/Country-only-cn-private.mmdb &&
     mmdb_hash=$(sha256sum /data/Country-only-cn-private.mmdb | grep -Eo "[a-zA-Z0-9]{64}" | head -1) &&
-    mmdb_down_hash=$(curl -s https://raw.githubusercontent.com/kkkgo/Country-only-cn-private.mmdb/main/Country-only-cn-private.mmdb.sha256sum | grep -Eo "[a-zA-Z0-9]{64}" | head -1) &&
+    mmdb_down_hash=$(grep -Eo "[a-zA-Z0-9]{64}" /data/mmdb_repo/Country-only-cn-private.mmdb.sha256sum | head -1) &&
+    rm -rf /data/mmdb_repo &&
     if [ "$mmdb_down_hash" = "$mmdb_hash" ]; then echo "mmdb_size pass."; else
         cp /mmdb_down_hash_error .
         exit
@@ -23,14 +25,15 @@ curl -sLo /data/top-1m.csv.zip http://s3-us-west-1.amazonaws.com/umbrella-static
     fi
 
 # Get the latest proxy.rules.: /data/global.nofilter.rules
-curl -sLo /data/gfwlist_base64.txt https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt &&
-    domains_size=$(wc -c <"/data/gfwlist_base64.txt") &&
-    if [ "$domains_size" -gt 100000 ]; then echo "domains_size pass."; else
+git clone --depth 1 -b master https://github.com/gfwlist/gfwlist /data/gfwlist_repo &&
+    cp /data/gfwlist_repo/list.txt /data/proxy.rules.txt &&
+    rm -rf /data/gfwlist_repo &&
+    domains_size=$(wc -l <"/data/proxy.rules.txt") &&
+    if [ "$domains_size" -gt 4000 ]; then echo "domains_size pass."; else
         echo "domains_size failed"
         cp /domains_size /
         exit
-    fi &&
-    base64 -d /data/gfwlist_base64.txt >/data/proxy.rules.txt
+    fi
 echo "" >>/data/proxy.rules.txt
 if [ -f /predata/global.hook.rules ]; then
     cat /predata/global.hook.rules >>/data/proxy.rules.txt
@@ -38,7 +41,9 @@ fi
 paopao-pref -inrule /data/proxy.rules.txt -outrule /data/global.nofilter.rules
 
 # Get the latest /data/cn.rules: direct + cn_mark
-curl -sLo /data/cn.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt &&
+git clone --depth 1 -b release https://github.com/Loyalsoldier/v2ray-rules-dat /data/v2ray_repo &&
+    cp /data/v2ray_repo/direct-list.txt /data/cn.txt &&
+    rm -rf /data/v2ray_repo &&
     domains_size=$(wc -c <"/data/cn.txt") &&
     if [ "$domains_size" -gt 50000 ]; then echo "domains_size pass."; else
         echo "domains_size failed"
