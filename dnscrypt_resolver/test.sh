07eeacc0e58ff090b1f147ca3ce3ba442a2c9dfb
kissshot <4530897+kkkgo@users.noreply.github.com>
2026-02-24 12:08:39 +0800

    clean

--- dnscrypt_resolver/test.sh
+++ dnscrypt_resolver/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd dnscrypt_resolver || exit 
-go mod init check&& go get -u&&go build -ldflags="-s -w" -o check check.go
+rm -rf check&&go mod init check&& go get -u&&go build -ldflags="-s -w" -o check check.go
 ./check
-rm -rf go.mod go.sum
+rm -rf go.mod go.sum check
